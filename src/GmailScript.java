e9572ad34279319b97f60f084c666875a897c2b9
Beimnet Zewdu <zbeimnet@gmail.com>
2019-06-22 21:36:30 +0300

    gmail login automation worked

--- src/GmailScript.java
+++ src/GmailScript.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,7 +22,13 @@ public class GmailScript {
 
         //signing into my email
         driver.findElement(By.id("identifierId")).sendKeys(myUsername);
-        driver.findElement(By.className("ZFr60d CeoRYc")).click();
+        driver.findElement(By.id("identifierNext")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 100);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+        WebElement elementpwd = driver.findElement(By.xpath("//input[@type='password']"));
+        elementpwd.sendKeys(myPassword);
+        driver.findElement(By.id("passwordNext")).click();
     }
 
     private static void writeToFile(String content) {
